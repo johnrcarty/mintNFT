58ab9657f2908dbd658433fb75a94ae6a72c326a
John Carty <johnrcarty@icloud.com>
2021-10-27 21:30:56 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+/**
+2	* @type import('hardhat/config').HardhatUserConfig
+3	*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+	   solidity: "0.8.0",
+	   defaultNetwork: "ropsten",
+	   networks: {
+	      hardhat: {},
+	      ropsten: {
+	         url: API_URL,
+	         accounts: [`0x${PRIVATE_KEY}`]
+	      }
+	   },
+	}
